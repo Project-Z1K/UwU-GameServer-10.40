09af61a63d06f596cceed23a35eb762a8b8cba01
Volcano-MP <149071205+Project-Z1K@users.noreply.github.com>
2024-03-08 16:26:16 +0330

    added Globals.h

---
+++ SulfurV3/Globals.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "hooks.h"
+
+namespace Globals
+{
+	static inline bool bSiphonEnabled = true;
+	static inline bool bLogProcessEvent = false;
+	static inline bool bCreative = false; // Playlist->bEnableCreativeMode
+	static inline bool bLateGame = false;
+	// static inline bool bMinimumPlayersToDropLS = 1;
+	static inline bool bPlayground = false;
+	static inline bool bRestarting = false;
+	static inline bool bNoMCP = true;
+	static int AmountOfRestarts = 0;
+}
